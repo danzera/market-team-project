e58c52bfc68d0eb6452542314162026c869e8d14
Kevin Dahlberg <nsu_tpt@hotmail.com>
2017-04-20 12:57:29 -0500

    updated DOM to have array from factory

--- client/scripts/controllers/marketController.js
+++ client/scripts/controllers/marketController.js
@@ -1,4 +1,5 @@
 myApp.controller('MarketController', ['MarketService', function(MarketService){
   let market = this;
   market.item = ['Apple', 'Banana', 'Orange'];
+  MarketService.marketItemsArray = market.marketItemsArray;
 }]);
