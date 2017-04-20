748a286c9139a3725e835b14b13914dd28e3247e
Dan Zera <dczera@gmail.com>
2017-04-20 09:25:17 -0500

    eliminate some browser errors

--- client/scripts/client.js
+++ client/scripts/client.js
@@ -1,33 +1,4 @@
 var myApp = angular.module('myApp', []);
-
-<<<<<<< HEAD
-// myApp.config(['$routeProvider', function($routeProvider){
-//     $routeProvider
-//         .when('/home', {
-//             templateUrl : 'views/templates/home.html',
-//             controller: 'HomeController',
-//             controllerAs: 'home'
-//         })
-//         .when('/blog', {
-//             templateUrl : 'views/templates/blog.html',
-//             controller: 'BlogController',
-//             controllerAs: 'blog'
-//         })
-//         .when('/menu', {
-//             templateUrl : 'views/templates/menu.html',
-//             controller: 'MenuController',
-//             controllerAs: 'menu'
-//         })
-//         .when('/contact', {
-//             templateUrl : 'views/templates/contact.html',
-//             controller: 'ContactController',
-//             controllerAs: 'contact'
-//         })
-//         .otherwise({
-//             redirectTo: 'home'
-//         });
-// }]);
-=======
 myApp.config(['$routeProvider', function($routeProvider){
     $routeProvider
         .when('/marketView', {
@@ -44,4 +15,3 @@ myApp.config(['$routeProvider', function($routeProvider){
             redirectTo: 'marketView'
         });
 }]);
->>>>>>> origin/feature-mvpviews
