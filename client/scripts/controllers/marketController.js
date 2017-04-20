5283de40d3bcb460f1eae88dce1dfb4cba868f98
Dan Zera <dczera@gmail.com>
2017-04-20 16:30:36 -0500

    add cart functionality

--- client/scripts/controllers/marketController.js
+++ client/scripts/controllers/marketController.js
@@ -1,18 +1,24 @@
 myApp.controller('MarketController', ['MarketService', function(MarketService){
   let market = this;
 
-  market.item = ['Apple', 'Banana', 'Orange', 'pears'];
-
   //create the cartItem will show to the dom
-market.cartItemArray = [{name: 'apple', quantity: 2, avePrice: 5},
-                 {name: 'Toaste', quantity: 0, avePrice: 10},
-                 {name: 'Books', quantity: 4, avePrice: 3.50}];
+// market.cartItemArray = [{name: 'apple', quantity: 2, avePrice: 5},
+//                  {name: 'Toaste', quantity: 0, avePrice: 10},
+//                  {name: 'Books', quantity: 4, avePrice: 3.50}];
 
 market.marketItemsArray = MarketService.marketItemsArray;
 console.log(market.marketItemsArray);
 
+market.cart = MarketService.cart;
+
+console.log("cart", market.cart);
+
+market.cartItemArray = market.cart.cartItemsArray;
+
  market.newCartItem = function (object){
   console.log('NG Click "clicked"', object);
+  market.cart.addItem(object);
+  console.log(market.cart);
 };
 
 
