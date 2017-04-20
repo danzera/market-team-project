e5abfb0d58edcba50783918d661ed10da77949f4
Dan Zera <dczera@gmail.com>
2017-04-20 10:10:12 -0500

    add constants and additional classes to the factory

--- client/scripts/factories/MarketService.js
+++ client/scripts/factories/MarketService.js
@@ -1,6 +1,28 @@
 myApp.factory('MarketService', function(){
   console.log("hi");
 
+  // constants
+  // market item prices
+  const INITIAL_PRICE = 5.00;
+  const MIN_PRICE = 0.50;
+  const MAX_PRICE = 9.99;
+  // price change min/max
+  const MIN_PRICE_CHANGE = 0.01;
+  const MAX_PRICE_CHANGE = 0.50;
+  // time between price changes
+  const PRICE_CHANGE_INTERVAL = 15000;
+
+  class Util {
+    constructor() {}
+
+    static randomNumber(min, max) {
+      return Math.floor(Math.random() * (1 + max - min) + min);
+    }
+  }
+  // setInterval(function() {
+  //   console.log("Hello");
+  // }, PRICE_CHANGE_INTERVAL);
+
   // parent class
   class MarketItem {
     constructor(name, price, category) {
@@ -25,10 +47,24 @@ myApp.factory('MarketService', function(){
     }
   }
 
+  class SmallElectronic extends MarketItem {
+    constructor(name, price) {
+      super(name, price, 'small electronic');
+    }
+  }
+
+  class Collectible extends MarketItem {
+    constructor(name, price) {
+      super(name, price, 'collectible');
+    }
+  }
+
   let papaya = new Fruit('papaya', 2);
   console.log(papaya);
 
   return {
-
+    Fruit,
+    SmallElectronic,
+    Collectible
   };
 });
--- server/public/scripts/controllers/marketController.js
+++ server/public/scripts/controllers/marketController.js
@@ -1,4 +1,10 @@
-myApp.controller('MarketController', ['DataService', function(DataService){
+myApp.controller('MarketController', ['MarketService', function(MarketService){
   let market = this;
-  market = ['Apple', 'Banana', 'Orange'];
+  market.Fruit = MarketService.Fruit;
+  market.SmallElectronic = MarketService.SmallElectronic;
+  market.marketArray = ['Apple', 'Banana', 'Orange'];
+  let mango = new market.Fruit('MANGO', 4);
+  console.log(mango);
+  let ELECTRICmango = new market.SmallElectronic('ELECTRIC MANGO', 4);
+  console.log(ELECTRICmango);
 }]);
