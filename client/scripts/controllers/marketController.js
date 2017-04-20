e2c54d2426740d0b37f6f2e5223c921f37f9f29e
EmilyH <EmilyHHLam@gmail.com>
2017-04-20 13:42:35 -0500

    add the cartitem on DOM

--- client/scripts/controllers/marketController.js
+++ client/scripts/controllers/marketController.js
@@ -1,4 +1,10 @@
 myApp.controller('MarketController', ['MarketService', function(MarketService){
   let market = this;
-  market.item = ['Apple', 'Banana', 'Orange'];
+  market.item = ['Apple', 'Banana', 'Orange', 'pears'];
+
+  //create the cartItem will show to the dom
+market.cartItemArray = [{name: 'apple', quantity: 2, avePrice: 5},
+                 {name: 'Toaste', quantity: 0, avePrice: 10},
+                 {name: 'Books', quantity: 4, avePrice: 3.50}];
+
 }]);
